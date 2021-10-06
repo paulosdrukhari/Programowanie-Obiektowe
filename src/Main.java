4826e2105a23448a8149f9450a0025e56341e818
paulosdrukhari <92029429+paulosdrukhari@users.noreply.github.com>
2021-10-06 14:11:51 +0200

    comit 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Styczen");
+        int a =   myObj.nextInt();;
+        System.out.println("Luty");
+        int b =   myObj.nextInt();
+        System.out.println("Marzec");
+        int c =   myObj.nextInt();;
+        System.out.println(a+b+c);
+    }
+}

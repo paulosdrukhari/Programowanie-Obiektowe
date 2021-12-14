956a68bcb06a65be2c6ea65b38ca0ab45dfb1118
paulosdrukhari <92029429+paulosdrukhari@users.noreply.github.com>
2021-12-14 21:43:36 +0100

    Update Teststudent.java

--- lab9/lab9java/Teststudent.java
+++ lab9/lab9java/Teststudent.java
@@ -7,9 +7,10 @@ public class Teststudent {
         ArrayList<Student> grupa = new ArrayList<Student>();
         grupa.add(new Student("Artas",LocalDate.of(1332,11,12),2.4));
         grupa.add(new Student("Artas",LocalDate.of(1332,1,12),4.21));
-        grupa.add(new Student("sarta",LocalDate.of(2000,1,11),2.53));
-        grupa.add(new Student("satraq",LocalDate.of(2000,1,11),4.45));
+        grupa.add(new Student("sarta",LocalDate.of(2137,12,21),2.53));
+        grupa.add(new Student("satraq",LocalDate.of(2137,12,21),4.45));
         grupa.add(new Student("Deptuła",LocalDate.of(2137,11,27),5.1));
+        System.out.print(grupa);
         Collections.sort(grupa);
         System.out.print(grupa);
     }
